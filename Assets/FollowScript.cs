76f513d0f36c65488ad83cc899892ad64318752d
Dragan <dragan.vule98@gmail.com>
2024-02-17 10:33:52 +0100

    Implemented smooth rotation flollow box

--- Assets/FollowScript.cs
+++ Assets/FollowScript.cs
@@ -5,19 +5,42 @@ using UnityEngine;
 public class Assistent : MonoBehaviour
 {
     public Transform playerBodyTransform;
+    public float rotSpeedMultiplier;
+
     //public Vector3Variable playerBodyPosition;
+    private Quaternion initRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.localRotation = playerBodyTransform.localRotation;
+        transform.localRotation = initRotation = playerBodyTransform.localRotation;
         transform.position = playerBodyTransform.position;
+        StartCoroutine(SmoothRotation());
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localRotation = playerBodyTransform.localRotation;
         transform.position = playerBodyTransform.position;
     }
+
+    IEnumerator SmoothRotation()
+    {
+        while (true)
+        {
+            Quaternion rotateTo = playerBodyTransform.localRotation;
+            float sumTime = 0.0f;
+
+            while (sumTime < 1.0f)
+            {
+                transform.localRotation = Quaternion.Slerp(initRotation, rotateTo, sumTime);
+                sumTime += Time.deltaTime * rotSpeedMultiplier;
+
+                yield return null;
+            }
+
+            initRotation = rotateTo;
+        }
+    }
+
 }
