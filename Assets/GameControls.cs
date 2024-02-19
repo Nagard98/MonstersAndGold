0f48d273bf083709f564d996bc3135a0afbccefe
Dragan <dragan.vule98@gmail.com>
2024-02-19 09:44:43 +0100

    Added start/end workout loading icon

--- Assets/GameControls.cs
+++ Assets/GameControls.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class GameControls : MonoBehaviour
 {
     public InputActionReference endWorkoutAction;
     public InputActionReference startWorkoutAction;
     public InputActionReference calibrate;
+    public float HoldDuration;
+    public Image loadCircle;
 
     public UnityEvent RightTriggerHeld;
     public UnityEvent LeftTriggerHeld;
@@ -20,6 +23,10 @@ public class GameControls : MonoBehaviour
         startWorkoutAction.action.performed += HeldLeftTrigger;
         calibrate.action.performed += Calibrate;
 
+        endWorkoutAction.action.started += AnimateCircleLoad;
+        startWorkoutAction.action.started += AnimateCircleLoad;
+
+
         endWorkoutAction.action.Disable();
     }
 
@@ -28,13 +35,39 @@ public class GameControls : MonoBehaviour
         Debug.Log("Calibrating...");
     }
 
+    private void AnimateCircleLoad(InputAction.CallbackContext obj)
+    {
+        StartCoroutine(AnimationCoroutine());
+    }
+
+    private IEnumerator AnimationCoroutine()
+    {
+        while (true)
+        {
+            if (startWorkoutAction.action.inProgress || endWorkoutAction.action.inProgress)
+            {
+                loadCircle.fillAmount += Mathf.Clamp01(Time.deltaTime / (HoldDuration));
+                yield return null;
+            }
+            else
+            {
+                loadCircle.fillAmount = 0.0f;
+                yield break;
+            }
+            
+        }
+
+    }
+
     // Update is called once per frame
     void Update()
     {
+
     }
 
     private void HeldLeftTrigger(InputAction.CallbackContext obj)
     {
+        loadCircle.fillAmount = 0.0f;
         Debug.Log("Held Left Trigger");
         LeftTriggerHeld.Invoke();
         startWorkoutAction.action.Disable();
@@ -43,6 +76,7 @@ public class GameControls : MonoBehaviour
 
     private void HeldRightTrigger(InputAction.CallbackContext obj)
     {
+        loadCircle.fillAmount = 0.0f;
         Debug.Log("Held Right Trigger");
         RightTriggerHeld.Invoke();
         endWorkoutAction.action.Disable();
